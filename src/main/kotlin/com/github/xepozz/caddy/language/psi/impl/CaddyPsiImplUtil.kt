7a74e7a2a86a9d32fbbe43af942e432758e5eb95
Dmitriy Derepko <xepozz@list.ru>
2025-12-15 07:10:43 +0400

    fix: broken heredoc

--- src/main/kotlin/com/github/xepozz/caddy/language/psi/impl/CaddyPsiImplUtil.kt
+++ src/main/kotlin/com/github/xepozz/caddy/language/psi/impl/CaddyPsiImplUtil.kt
@@ -3,9 +3,7 @@ package com.github.xepozz.caddy.language.psi.impl
 import com.github.xepozz.caddy.language.psi.CaddyAddress
 import com.github.xepozz.caddy.language.psi.CaddyHeredoc
 import com.github.xepozz.caddy.language.psi.CaddyTypes
-import com.intellij.psi.LiteralTextEscaper
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiLanguageInjectionHost
 import com.intellij.psi.PsiReference
 import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
 
@@ -13,16 +11,6 @@ class CaddyPsiImplUtil {
     companion object {
         @JvmStatic
         fun getValue(element: CaddyAddress): String? = element.text
-
-        @JvmStatic
-        fun isValidHost(element: CaddyHeredoc): Boolean = element.isValidHost
-
-        @JvmStatic
-        fun updateText(element: CaddyHeredoc, newText: String): PsiLanguageInjectionHost = element.updateText(newText)
-
-        @JvmStatic
-        fun createLiteralTextEscaper(element: CaddyHeredoc): LiteralTextEscaper<out PsiLanguageInjectionHost> =
-            element.createLiteralTextEscaper()
 
         @JvmStatic
         fun getValue(element: CaddyHeredoc): String? =
